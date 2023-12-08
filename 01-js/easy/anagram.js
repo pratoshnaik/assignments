bdaa460d14bcd9a68ea70fb48a8216bae7de43d5
Pratosh Naik <p.naik@sap.com>
2023-12-08 12:19:53 +0530

    Easy Assignment Cohort 2

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+    let nstr1 = str1.split("");
+    let nstr2 = str2.split("");
+    if(nstr1.length != nstr2.length){
+      return false;
+    }
+    if(nstr1.sort().join('')===nstr2.sort().join('')){
+      return true;
+    }
 }
+
 
 module.exports = isAnagram;
